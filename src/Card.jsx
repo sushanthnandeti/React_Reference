e2d56d990e026c7ab2e40406a1097b409074cf5d
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-05-27 02:27:02 -0400

    Cards

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+
+function Card() {
+    
+    return (
+        <div className="card">
+            <img className = "card-image" src = "https://images-cdn.bridgemanimages.com/api/1.0/image/600wm.REU.78319570.7055475/7588208.jpg"  alt = "profile picture" /> 
+            <h2 className = "card-title"> Sushanth </h2>
+            <p className = "card-text"> I am a software engineer </p>
+
+        </div>
+    );
+}
+
+
+export default Card;
